d969f5c8c18ebad782bd378c3ac50b0a1a4f1650
lifuyuan <lifuyuan33@gmail.com>
2021-03-29 13:18:56 +0800

    update .gitignore

--- main/models.py
+++ main/models.py
@@ -115,7 +115,7 @@ class Order(MongoModel):
         return sum([item.QuantityOrdered for item in self.order_items()])
 
     def order_amount(self):
-        return sum([float(item.ItemPrice.get('Amount', 0)) for item in self.order_items()])
+        return float(self.OrderTotal.get('Amount', 0))
 
 
 # 订单商品
